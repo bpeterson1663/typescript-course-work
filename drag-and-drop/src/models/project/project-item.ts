bb912276173817ec474720ab609e245be1311b67
Brady Peterson <bpeterson1663@gmail.com>
2021-02-14 08:01:25 -0600

    refactor(file structure): abstract out to separate files using namespace

---
+++ drag-and-drop/src/models/project/project-item.ts
@@ -0,0 +1,41 @@
+/// <reference path="../../models/component.ts" />
+/// <reference path="../../helpers/decorators.ts" />
+namespace App {
+    export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+        private project: Project
+    
+        get persons(): string {
+            if(this.project.people === 1){
+                return '1 person assigned';
+            }else {
+                return `${this.project.people} persons assigned`
+            }
+        }
+    
+        constructor(hostId: string, project: Project) {
+            super('single-project', hostId, false, project.id)
+            this.project = project
+            this.configure()
+            this.renderContent()
+        }
+        @autobind
+        dragStartHandler(event: DragEvent) { 
+            event.dataTransfer!.setData('text/plain', this.project.id)
+            event.dataTransfer!.effectAllowed = 'move'
+        }
+    
+        @autobind
+        dragEndHandler(_: DragEvent) { }
+    
+        configure() {
+            this.element.addEventListener('dragstart', this.dragStartHandler)
+            this.element.addEventListener('dragend', this.dragEndHandler)
+        }
+    
+        renderContent() {
+            this.element.querySelector('h2')!.textContent = this.project.title
+            this.element.querySelector('h3')!.textContent = this.persons
+            this.element.querySelector('p')!.textContent = this.project.description
+        }
+    }
+}
